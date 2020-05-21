3088ccc2e8be1cdb47904aaa63dfd6bc0719f1d5
Younhong <younhong@kakao.com>
2020-05-21 21:25:19 +0900

    indent again

--- younhong/src/script2.sh
+++ younhong/src/script2.sh
@@ -3,18 +3,18 @@ num=$((RANDOM%100 + 1))
 echo "Guess a number (1-100)?"
 read input
 if [ $input -lt 1 ]; then
-echo "Input number higher than 0"
+    echo "Input number higher than 0"
 elif [ $input -gt 100 ]; then
-echo "Input number lower than 100"
+    echo "Input number lower than 100"
 else
-while [ $input -ne $num ] 
-do
-    if [ $input -lt $num ]; then
-    echo "Lower"
-    elif [ $input -gt $num ]; then
-    echo "Higher"
-    fi
-    read input
-done
+    while [ $input -ne $num ] 
+    do
+        if [ $input -lt $num ]; then
+            echo "Lower"
+        elif [ $input -gt $num ]; then
+            echo "Higher"
+        fi
+        read input
+    done
 echo "Correct! You pick the right number!"
 fi
