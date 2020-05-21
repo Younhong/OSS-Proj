5f9cf1972d0aec02de782465b84a2f4e0ecec11f
Younhong <younhong@kakao.com>
2020-05-21 18:55:08 +0900

    indent

--- younhong/src/script3.sh
+++ younhong/src/script3.sh
@@ -3,18 +3,18 @@
 prev_first=0
 prev_second=1
 if [ $1 -eq 0 ]; then 
- echo "n = $1"
- echo "fib($1) = $prev_first"
+    echo "n = $1"
+    echo "fib($1) = $prev_first"
 elif [ $1 -eq 1 ]; then
- echo "n = $1" 
- echo "fib($1) = $prev_second" 
+    echo "n = $1" 
+    echo "fib($1) = $prev_second" 
 else
- for ((i = 0; i < $1-1; i++))
- do
- curr=$((prev_first+prev_second))
- prev_first=$prev_second
- prev_second=$curr
- done
- echo "n = $1"
- echo "fib($1) = $curr"
+    for ((i = 0; i < $1-1; i++))
+    do
+        curr=$((prev_first+prev_second))
+        prev_first=$prev_second
+        prev_second=$curr
+    done
+    echo "n = $1"
+    echo "fib($1) = $curr"
 fi
